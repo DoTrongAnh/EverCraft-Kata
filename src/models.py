7c31e4f5f1862c2844e09b543ac98ceaf4d6d214
Do Trong Anh <anh.trong.do.1994@gmail.com>
2024-07-05 13:10:36 -0600

    Fix flake8

--- src/models.py
+++ src/models.py
@@ -1,11 +1,11 @@
 class Character:
-	name: str
+    name: str
 
-	def __init__(self, **kwargs):
-		self.set_name(kwargs.get('name', ''))
+    def __init__(self, **kwargs):
+        self.set_name(kwargs.get('name', ''))
 
-	def set_name(self, name):
-		self.name = name
+    def set_name(self, name):
+        self.name = name
 
-	def get_name(self):
-		return self.name
+    def get_name(self):
+        return self.name
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,6 +1,7 @@
 from src.models import Character
 
+
 def test_character_name():
-	name_to_assign = 'Nathan'
-	nathan_character = Character(name=name_to_assign)
-	assert nathan_character.get_name() == name_to_assign
+    name_to_assign = 'Nathan'
+    nathan_character = Character(name=name_to_assign)
+    assert nathan_character.get_name() == name_to_assign
