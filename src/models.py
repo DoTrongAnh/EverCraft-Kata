2e0a086b71664ee10222500f19244d27a4afb28b
Do Trong Anh <anh.trong.do.1994@gmail.com>
2024-07-05 13:06:26 -0600

    Add Character model and test

---
+++ src/models.py
@@ -0,0 +1,11 @@
+class Character:
+	name: str
+
+	def __init__(self, **kwargs):
+		self.set_name(kwargs.get('name', ''))
+
+	def set_name(self, name):
+		self.name = name
+
+	def get_name(self):
+		return self.name
---
+++ tests/test_character.py
@@ -0,0 +1,6 @@
+from src.models import Character
+
+def test_character_name():
+	name_to_assign = 'Nathan'
+	nathan_character = Character(name=name_to_assign)
+	assert nathan_character.get_name() == name_to_assign
